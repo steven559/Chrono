eadefd87016ba16eda2379413e77abed35897fdc
Stevens Dorchies <Trocme11>
2018-11-20 15:23:09 +0100

    Version1

--- count.js
+++ count.js
@@ -1,18 +1,15 @@
-
 var seconds = document.getElementById("seconds");
-var heure=document.getElementById("heur");
-var reste = reste%3600;
-var time = reste%60;
+var heure = document.getElementById("heur");
+var reste = reste % 3600;
+var time = reste % 60;
 var minutes = Math.floor(reste / 60);
 var stop;
-var heur=Math.floor(time / 3600);
-var enminute =document.getElementById("minute");
+var heur = Math.floor(time / 3600);
+var enminute = document.getElementById("minute");
 
 
-
-
-var stopCountUp = function(){
-
+var stopCountUp = function () {
+    document.getElementById("button").style.visibility = "visible";
     clearTimeout(stop);
 
 };
@@ -20,58 +17,53 @@ var stopButton = document.getElementById("stop-button");
 stopButton.addEventListener("click", stopCountUp);
 
 
+var but = document.getElementById("but");
+var inp = document.getElementById("tim");
 
-
-
-
-var but=document.getElementById("but");
-var inp=document.getElementById("tim");
-
-var input=document.getElementById("minut");
-but.addEventListener("click",function(){
-    if(inp.value>=60){
-        inp.value="";
-        document.getElementById("ereur").innerHTML="veuillez entrer un nombre inferieur a 59";
+var input = document.getElementById("minut");
+but.addEventListener("click", function () {
+    if (inp.value >= 60) {
+        inp.value = "";
+        document.getElementById("ereur").innerHTML = "veuillez entrer un nombre inferieur a 59";
     }
-   else{
-        document.getElementById("ereur").style.visibility="hidden";
+    else {
+        document.getElementById("ereur").style.visibility = "hidden";
     }
 
     minutes = Math.floor(reste / 60);
-time=inp.value;
-    minutes=input.value;
-    heur=Math.floor(time / 3600);
-    heur=Math.floor(minutes/60);
+    time = inp.value;
+    minutes = input.value;
+    heur = Math.floor(time / 3600);
+    heur = Math.floor(minutes / 60);
     heure.innerHTML = heur;
-    reste = time%3600;
-    minutes=minutes%60;
+    reste = time % 3600;
+    minutes = minutes % 60;
 
 
+    enminute.innerHTML = minutes;
+    reste = reste % 60;
 
-        enminute.innerHTML = minutes;
-     reste = reste%60;
-
-     clearTimeout(stop);
+    clearTimeout(stop);
     stop = setTimeout(countUp, 1000);
-    time=reste;
-  //  seconds.innerHTML=reste--;
+    time = reste;
+    //  seconds.innerHTML=reste--;
 
     console.log(time);
 
 
-
 });
 
-var countUp =  function() {
-    stop=   setTimeout(countUp,1000);
+var countUp = function () {
+    stop = setTimeout(countUp, 1000);
     stop;
 
     enminute.innerHTML = minutes;
+
     seconds.innerHTML = reste--;
 
 
     console.log(minutes);
-    if(reste<=0 && minutes>0) {
+    if (reste <= 0 && minutes > 0) {
         reste = 59;
         minutes--;
         enminute.innerHTML = minutes;
@@ -80,38 +72,37 @@ var countUp =  function() {
 
         console.log();
     }
-    if(minutes<=0&&heur>0 ){
-        minutes=60;
+    if (minutes <= 0 && heur > 0) {
+        minutes = 60;
         heur--;
-        heure.innerHTML=heur;
-        enminute.innerHTML=minutes;
+        heure.innerHTML = heur;
+        enminute.innerHTML = minutes;
     }
-if(reste<=-1 && minutes<=0){
-    clearTimeout(stop);
-}
+    if (reste <= -1 && minutes <= 0) {
+        clearTimeout(stop);
+    }
 
 
-
-
-
+    document.getElementById("button").style.visibility = "hidden";
 
 };
 
 
+var bout = document.getElementById("bout");
+bout.addEventListener("click", function () {
 
-var bout=document.getElementById("bout");
-bout.addEventListener("click",function(){
 
-
-clearTimeout(stop);
+    clearTimeout(stop);
     console.log(stop);
-    time=0;
-    minutes=0;
-    heur=0;
+    time = 0;
+    minutes = 0;
+    heur = 0;
     enminute.innerHTML = minutes;
-    seconds.innerHTML=time;
-
+    seconds.innerHTML = time;
+    heure.innerHTML = heur;
 
 });
-var button=document.getElementById("button");
-button.addEventListener("click",countUp);
+var button = document.getElementById("button");
+button.addEventListener("click", countUp);
+
+
